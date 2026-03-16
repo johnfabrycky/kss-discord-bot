998779dbdae37ff8fd0e185de4b481b74b362e4c
johnfabrycky <157093716+johnfabrycky@users.noreply.github.com>
2026-03-16 12:36:46 -0500

    Change cleanup task time to 12:45 AM

--- cogs/lates.py
+++ cogs/lates.py
@@ -36,7 +36,7 @@ class Lates(commands.Cog):
         return None
 
     # Anchors the task to run every day at 11:00 PM
-    @tasks.loop(time=time(hour=12, minute=35, second=0))
+    @tasks.loop(time=time(hour=12, minute=45, second=0))
     async def cleanup_temporary_lates(self):
         """Deletes all temporary lates on Saturday night."""
         now = datetime.now(local_tz)
